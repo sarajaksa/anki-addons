eabc128e7869a31eeb984f7aae4a309034dfa6ce
Sara Jakša <sarajaksa@gmail.com>
2017-07-25 09:56:54 +0200

    Ported Edit Buttons to Anki 2.1

--- edit-buttons.py
+++ edit-buttons.py
@@ -4,86 +4,97 @@
 import re
 
 from aqt.editor import Editor, _html
-from anki.hooks import wrap
+from anki.hooks import addHook
 
 from aqt.qt import *
 
-def addButtonSize(editor):
-    addSizes(editor)
+jsFont = """
+	var e = document.getElementById("FontName");
+	var font = e.options[e.selectedIndex].value;
+	var front = '<span style="font-family:' + font + '" >'
+	var back = '</span>'
+	var s = window.getSelection();
+	var r = s.getRangeAt(0);
+	var content = r.cloneContents();
+	var span = document.createElement("span")
+	span.appendChild(content);
+	var new_ = wrappedExceptForWhitespace(span.innerHTML, front, back);
+	setFormat("inserthtml", new_);
+	if (!span.innerHTML) {
+	    // run with an empty selection; move cursor back past postfix
+	    r = s.getRangeAt(0);
+	    r.setStart(r.startContainer, r.startOffset - back.length);
+	    r.collapse(true);
+	    s.removeAllRanges();
+	    s.addRange(r);
+	    }
+	 """
+         
+jsSize = """
+	var e = document.getElementById("FontSize");
+	var num = e.options[e.selectedIndex].value;
+	var front = '<span style="font-size:' + num + 'px" >'
+	var back = '</span>'
+	var s = window.getSelection();
+	var r = s.getRangeAt(0);
+	var content = r.cloneContents();
+	var span = document.createElement("span")
+	span.appendChild(content);
+	var new_ = wrappedExceptForWhitespace(span.innerHTML, front, back);
+	setFormat("inserthtml", new_);
+	if (!span.innerHTML) {
+		// run with an empty selection; move cursor back past postfix
+		r = s.getRangeAt(0);
+		r.setStart(r.startContainer, r.startOffset - back.length);
+		r.collapse(true);
+		s.removeAllRanges();
+		s.addRange(r);
+		}
+	"""
     
-def addButtonFont(editor):
-    addFonts(editor)
+jsCase = """
+	var s = window.getSelection();
+	var r = s.getRangeAt(0);
+	var content = r.cloneContents();
+	var c = content.textContent
+	if (c!=c.toUpperCase()) {
+	var c = c.toUpperCase();
+	} else {
+	var c = c.toLowerCase();
+	}
+	r.deleteContents();
+	r.insertNode(document.createTextNode(c));
+	"""
 
-def addButtonCase(editor):
-    editor._addButton("changeUpper", lambda: containerCase(editor), key=_("Shift+F3"), tip=_(u"Changes case of the text (Shift+F3)"), text=_(u"Cc"))
+def addButtonSize(buttons, editor):
+    editor._links['size'] = changeSize
+    allFontSizes = ""
+    for i in [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 20, 22, 24, 26, 28, 32, 36, 40, 44, 48, 54, 60, 66, 72, 80, 88, 96]:
+        allFontSizes = allFontSizes + '<option>' + str(i) + '</option>'
+    return buttons + ['<select name="FontSize" id=FontSize tabindex=1 title="Change Font" onChange="pycmd(\'size\');return false;">' + allFontSizes + '</select>\n\n']
     
-def addSizes(self):
-        sizeOptions = QComboBox()
-        sizes = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 20, 22, 24, 26, 28, 32, 36, 40, 44, 48, 54, 60, 66, 72, 80, 88, 96]
-        for size in sizes:
-            sizeOptions.addItem(str(size))
-        sizeOptions.activated.connect(lambda: containerSize(self, sizeOptions.currentText()))
-        self.iconsBox.addWidget(sizeOptions)
-    
-def addFonts(self):
-    fontOptions = QComboBox()
-    allfonts = QFontDatabase()
-    fonts = allfonts.families()
-    fonts = [re.sub(r'\[.*?\]', '', font).strip() for font in fonts]
-    for font in fonts:
-        fontOptions.addItem(str(font))
-    fontOptions.activated.connect(lambda: containerFont(self, fontOptions.currentText()))
-    self.iconsBox.addWidget(fontOptions)
-   
-def containerSize(editor, size):
-    selection = editor.web.selectedText()
-    if not selection:
-        return
-    editor.web.eval("wrap('[spreminjanjevelikosti1]', '[spreminjanjevelikosti2]');")
-    html = editor.note.fields[editor.currentField]
-    text = html.replace("[spreminjanjevelikosti1]", "<span style='font-size: " + str(size) + "px;'>")
-    text = text.replace("[spreminjanjevelikosti2]", "</span>")
-    saveChanges(editor, text)
-    
-def containerFont(editor, font):
-    html = editor.note.fields[editor.currentField]
-    selection = editor.web.selectedText()
-    if not selection:
-        return
-    editor.web.eval("wrap('[spreminjanjepisave1]', '[spreminjanjepisave2]');")
-    html = editor.note.fields[editor.currentField]
-    text = html.replace("[spreminjanjepisave1]", "<span style='font-family: " + font + ";'>")
-    text = text.replace("[spreminjanjepisave2]", "</span>")
-    saveChanges(editor, text)
+def addButtonFont(buttons, editor):
+    editor._links['font'] = changeFont
+    allFonts =  QFontDatabase()
+    allFonts = allFonts.families()
+    allFontsSelection = ""
+    for font in allFonts:
+        allFontsSelection = allFontsSelection + '<option>' + font + '</option>'
+    return buttons + ['<select name="FontName" id=FontName tabindex=2 style="width:80px" title="Change Font" onChange="pycmd(\'font\');return false;">' + allFontsSelection + '</select>\n\n']
 
-def containerCase(editor):
-    selection = editor.web.selectedText()
-    if not selection:
-        return
-    editor.web.eval("wrap('[spreminjanjecrk1]', '[spreminjanjecrk2]');")
-    html = editor.note.fields[editor.currentField]
-    matching = r"\[spreminjanjecrk1\](.*?)\[spreminjanjecrk2\]"
-    selection = re.findall(matching, html)[0]
-    replace = changeTextCase(selection)
-    start = html.find("[spreminjanjecrk1]") + len("[spreminjanjecrk1]")
-    text = html[:start] + replace + html[start + len(replace):]
-    text = text.replace("[spreminjanjecrk1]", "")
-    text = text.replace("[spreminjanjecrk2]", "")
-    saveChanges(editor, text)
+def addButtonCase(buttons, editor):
+    editor._links['case'] = changeCase
+    return buttons + [editor._addButton("iconname", "case","tooltip")]
     
-def changeTextCase(text):
-    if text.isupper():
-        return text.lower()
-    return text.upper()
+def changeSize(editor):
+    editor.web.eval(jsSize)
     
-def saveChanges(editor, text):
-    editor.note.fields[editor.currentField] = text
-    editor.loadNote()
-    editor.web.setFocus()
-    editor.saveNow()
-    editor.web.setFocus()
-    editor.web.eval("focusField(%d);" % editor.currentField)
+def changeFont(editor):
+    editor.web.eval(jsFont)
     
-Editor.setupButtons = wrap(Editor.setupButtons, addButtonFont)
-Editor.setupButtons = wrap(Editor.setupButtons, addButtonSize)
-Editor.setupButtons = wrap(Editor.setupButtons, addButtonCase)
+def changeCase(editor):
+    editor.web.eval(jsCase)
+    
+addHook("setupEditorButtons", addButtonFont)
+addHook("setupEditorButtons", addButtonSize)
+addHook("setupEditorButtons", addButtonCase)
